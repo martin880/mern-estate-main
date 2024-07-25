4c5b627a83cfac1776a7605f5b0a72eed051410d
Martin Halomoan Lumbangaol <62680881+martin880@users.noreply.github.com>
2024-07-25 10:33:35 +0700

    Update : Update UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -50,15 +50,15 @@ export default function Home() {
       {/* top */}
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
-          Find your next <span className="text-slate-500">perfect</span>
+          Discover your next <span className="text-slate-500">ideal</span>
           <br />
-          place with ease
+          location effortlessly
         </h1>
-        <div className="text-gray-400 text-xs sm:text-sm">
-          Martin Estate is the best place to find your next perfect place to
-          live.
+        <div className="text-gray-500 text-xs sm:text-sm">
+          Martin Estate is the ultimate destination to find your next ideal
+          home.
           <br />
-          We have a wide range of properties for you to choose from.
+          We offer a diverse selection of properties for your consideration.
         </div>
         <Link
           to={"/search"}
