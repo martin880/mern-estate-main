c0349b7901790d9d5f8d76d1b427d2af2c4d32cd
Martin Halomoan Lumbangaol <62680881+martin880@users.noreply.github.com>
2025-01-06 10:48:30 +0700

    Update tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
+import lineClamp from "@tailwindcss/line-clamp";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [
-    require("@tailwindcss/line-clamp"),
-    // ...
+    lineClamp,
+    // Tambahkan plugin lain jika diperlukan
   ],
 };
