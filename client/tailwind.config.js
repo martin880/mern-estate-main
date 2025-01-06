71ce643a21286ae8c21db8453627bcc190b4ce88
Martin Halomoan Lumbangaol <62680881+martin880@users.noreply.github.com>
2025-01-06 12:10:17 +0700

    Update

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,27 +1,27 @@
-// /* eslint-disable no-undef */
-// /** @type {import('tailwindcss').Config} */
-// import lineClamp from "@tailwindcss/line-clamp";
-
-// export default {
-//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [
-//     lineClamp,
-//     // Tambahkan plugin lain jika diperlukan
-//   ],
-// };
-
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
+import lineClamp from "@tailwindcss/line-clamp";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [
-    // Hapus baris berikut karena sudah termasuk secara default di Tailwind CSS v3.3
-    // require('@tailwindcss/line-clamp'),
+    lineClamp,
+    // Tambahkan plugin lain jika diperlukan
   ],
 };
+
+// /* eslint-disable no-undef */
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     // Hapus baris berikut karena sudah termasuk secara default di Tailwind CSS v3.3
+//     // require('@tailwindcss/line-clamp'),
+//   ],
+// };
